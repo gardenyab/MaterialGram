f78daf40e816440f35136bbecc94b2f3f043b319
gardenyab <gardenyablochnoy@gmail.com>
2026-02-08 14:35:43 +0200

    Ok

---
+++ app/src/main/java/com/gardendev/materialgram/RegisterPage2.kt
@@ -0,0 +1,19 @@
+package com.gardendev.materialgram
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterPage2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
